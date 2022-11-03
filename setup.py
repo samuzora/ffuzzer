681460937a64a7995570cb8c81739f75bab68692
samuzora <lucastanyj@gmail.com>
2022-11-03 20:32:13 +0800

    release 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='ffuzzer',
-    version='0.1.3',
+    description='ffuzzer is a CLI tool that makes fuzzing format string offsets easy, especially relevant for full RELRO format string challenges, where you want to leak as much info from the binary as you can.',
+    version='0.1.4.post0',
     license='MIT',
     author='samuzora',
     author_email='lucastanyj@gmail.com',
@@ -13,9 +14,11 @@ setup(
     url='https://github.com/samuzora/ffuzzer',
     install_requires=[
         'Click',
-        'pwntools'
+        'pwntools',
+        'rich',
+        'regex'
     ],
-    python_requires='>=3',
+    python_requires='>=3.9',
     entry_points={
         'console_scripts': [
             'ffuzzer=ffuzzer:cli',
